11fdc5eb9beac9245d3bdfd4a2b3f2ae3f4170fe
Archareux <72795035+Archareuxxx@users.noreply.github.com>
2025-01-15 08:51:26 +0700

    Update github_manager.py

--- App/github_manager.py
+++ App/github_manager.py
@@ -14,13 +14,13 @@ class GitHubManager:
         if not os.path.exists(clone_path):
             subprocess.run(["git", "clone", url, clone_path])
             self.db.insert_repo(repo_name, url, clone_path)
-            print(f"Repository {repo_name} berhasil di-clone.")
+            print(f"Repository {repo_name} succesfully clonned.")
         else:
-            print(f"Repository {repo_name} sudah ada.")
+            print(f"Repository {repo_name} already exist.")
 
     def list_repos(self):
         repos = self.db.fetch_all()
-        print("Daftar Repository:")
+        print("Repositori List:")
         for repo in repos:
             print(f"[{repo[0]}] {repo[1]} - {repo[2]} (Path: {repo[3]})")
 
@@ -30,12 +30,12 @@ class GitHubManager:
             if repo[0] == int(repo_id):
                 subprocess.run(["rm", "-rf", repo[3]])
                 self.db.delete_repo(repo_id)
-                print(f"Repository {repo[1]} berhasil dihapus.")
+                print(f"Repository {repo[1]} Succesfully deleted.")
                 return
-        print("ID Repository tidak ditemukan.")
+        print("ID Repository not found.")
 
     def update_all_repos(self):
         repos = self.db.fetch_all()
         for repo in repos:
             subprocess.run(["git", "-C", repo[3], "pull"])
-            print(f"Repository {repo[1]} diperbarui.")
+            print(f"Repository {repo[1]} updated.")
